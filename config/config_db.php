e5a6bc18514e6778d1a68194274162c8037cb4dc
JMarvis <jmarquez@fotex.es>
2018-04-26 11:52:08 +0200

    config_db

--- config/config_db.php
+++ config/config_db.php
@@ -1,11 +1,7 @@
 <?php
 class DB extends DBmysql {
    public $dbhost     = 'localhost';
-<<<<<<< HEAD
-   public $dbuser     = 'daniel.torvisco';
-=======
    public $dbuser     = 'root';
->>>>>>> 293dd763a5806f2c149f2b740caeac16623e7b9d
    public $dbpassword = 'chkdsk2002';
    public $dbdefault  = 'glpibovis';
 }
